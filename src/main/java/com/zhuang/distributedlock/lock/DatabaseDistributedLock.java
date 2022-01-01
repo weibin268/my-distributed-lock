d60e64b53e9d004081f8982102241cf33cf8f49a
zwb <binweizhuang@hotmail.com>
2022-01-01 19:56:48 +0800

    aa

--- src/main/java/com/zhuang/distributedlock/lock/DatabaseDistributedLock.java
+++ src/main/java/com/zhuang/distributedlock/lock/DatabaseDistributedLock.java
@@ -3,14 +3,10 @@ package com.zhuang.distributedlock.lock;
 
 import com.zhuang.distributedlock.config.LockProperties;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 
 @Slf4j
 public class DatabaseDistributedLock implements Lock<String>, InitializingBean {
-
-    private Logger logger = LoggerFactory.getLogger(DatabaseDistributedLock.class);
 
     @Override
     public String lock(String lock, LockProperties lockProperties) {
